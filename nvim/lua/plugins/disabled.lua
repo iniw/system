faed67936ba68d02e4078055ce1efe9f21b08711
Vinicius Deolindo <git@vini.cat>
2024-08-27 16:40:33 -0300

    workaround some zellij bugs

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -8,4 +8,11 @@ return {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+
+  {
+    -- FIXME: Enable this again
+    -- https://github.com/zellij-org/zellij/issues/3208
+    "folke/noice.nvim",
+    enabled = false,
+  },
 }
