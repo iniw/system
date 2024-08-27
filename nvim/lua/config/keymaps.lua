d1c4b06e266cd43a7d99fea75ac095517ef587ea
vini <andrade.vinicius934@gmail.com>
2024-08-26 22:53:55 -0300

    sync lazyvim and lazy-lock

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "<C-o>", "<Left><C-o>")
-vim.keymap.set("n", "gad", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>")
+
+vim.keymap.set("n", "gad", function()
+  require("telescope.builtin").lsp_definitions({
+    jump_type = "vsplit",
+    attach_mappings = function(_, map)
+      map({ "i", "n" }, "<CR>", require("telescope.actions").select_vertical)
+      return true
+    end,
+  })
+end, { noremap = true, silent = true, desc = "Go to definition in a new window" })
 
 -- Quick jump to the Nth window/tab
 for i = 1, 6 do
