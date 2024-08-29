015657edb0fda72d8c8b5df2ac8beda91a99131a
Vinicius Deolindo <git@vini.cat>
2024-08-29 11:01:12 -0300

    filter annoying jdtls messages through noice

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -112,9 +112,26 @@ return {
   {
     "folke/noice.nvim",
     opts = {
-      lsp = {
-        progress = {
-          enabled = false,
+      routes = {
+        {
+          filter = {
+            event = "lsp",
+            kind = "progress",
+            cond = function(message)
+              local client = vim.tbl_get(message.opts, "progress", "client")
+              if client ~= "jdtls" then
+                return false
+              end
+
+              local content = vim.tbl_get(message.opts, "progress", "message")
+              if content == nil then
+                return false
+              end
+
+              return string.find(content, "Validate") or string.find(content, "Publish")
+            end,
+          },
+          opts = { skip = true },
         },
       },
     },
@@ -134,8 +151,6 @@ return {
     },
   },
 
-  "mg979/vim-visual-multi",
-
   {
     "echasnovski/mini.ai",
     config = true,
@@ -152,4 +167,6 @@ return {
       silent = true,
     },
   },
+
+  "mg979/vim-visual-multi",
 }
