57a12cf449b90de15d02fb2ef72f046d2d4660c0
Vinicius Deolindo <git@vini.cat>
2024-10-08 22:41:21 -0300

    flake: make `mac-app-util`'s `nixpkgs` follow the flake's

--- flake.nix
+++ flake.nix
@@ -14,7 +14,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    mac-app-util.url = "github:hraban/mac-app-util";
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
