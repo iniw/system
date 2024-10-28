cd198e0781944cb082be7dfd418d4943d513ff24
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2024-10-28 12:46:14 -0300

    nvim: improve todo-comments

---
+++ home/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      highlight = {
+        keyword = "wide_fg",
+        -- Don't highlight the actual comment text, just the keyword.
+        after = "",
+      },
+    },
+  },
+}
