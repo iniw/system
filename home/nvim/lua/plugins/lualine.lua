f2bf5032694feeea263e030ccf32b733da20bd25
Vinicius Deolindo <git@vini.cat>
2024-12-13 00:46:50 -0300

    nvim: only partially override lualine's config
    nvim: update lockfile

--- home/nvim/lua/plugins/lualine.lua
+++ home/nvim/lua/plugins/lualine.lua
@@ -1,51 +1,14 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    -- Based on the snippet at: https://www.lazyvim.org/plugins/ui#lualinenvim
-    opts = function()
-      -- PERF: we don't need this lualine require madness 🤷
-      local lualine_require = require("lualine_require")
-      lualine_require.require = require
-
-      vim.o.laststatus = vim.g.lualine_laststatus
-
-      local opts = {
-        options = {
-          theme = "auto",
-          globalstatus = vim.o.laststatus == 3,
-          disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
-            LazyVim.lualine.root_dir(),
-            { LazyVim.lualine.pretty_path() },
-          },
-          lualine_x = {},
-          lualine_y = {
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.command.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-              color = function() return { fg = Snacks.util.color("Statement") } end,
-            },
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.mode.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              color = function() return { fg = Snacks.util.color("Constant") } end,
-            },
-          },
-          lualine_z = {
-            { "progress", separator = " ", padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-        },
-        extensions = { "neo-tree" },
+    opts = function(_, opts)
+      opts.sections.lualine_c = {
+        LazyVim.lualine.root_dir(),
+        { LazyVim.lualine.pretty_path() },
       }
 
-      return opts
+      opts.sections.lualine_x = {}
+      opts.sections.lualine_z = {}
     end,
   },
 }
