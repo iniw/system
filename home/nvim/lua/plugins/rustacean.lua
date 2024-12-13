61491344bd34da31f9e55ccd5234ec1f50f1c613
Vinicius Deolindo <git@vini.cat>
2024-12-12 23:23:03 -0300

    nvim: improve rustacean.nvim config

--- home/nvim/lua/plugins/rustacean.lua
+++ home/nvim/lua/plugins/rustacean.lua
@@ -1,28 +1,26 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    init = function()
-      vim.g.rustaceanvim = {
-        server = {
-          default_settings = {
-            ["rust-analyzer"] = {
-              files = {
-                excludeDirs = {
-                  -- Exclude these directories otherwise ra gets utterly demolished attempting to analyze the entirety of nixpkgs.
-                  ".direnv/",
-                  ".git/",
-                  ".github/",
-                  ".gitlab",
-                  "bin",
-                  "node_modules",
-                  "target",
-                  "venv",
-                },
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            files = {
+              excludeDirs = {
+                ".direnv/",
+                ".git/",
+                ".github/",
+                ".gitlab",
+                "bin",
+                "node_modules",
+                "target",
+                "venv",
+                ".venv",
               },
             },
           },
         },
-      }
-    end,
+      },
+    },
   },
 }
