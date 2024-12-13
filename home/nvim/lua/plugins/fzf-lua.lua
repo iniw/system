5ebbe5bf5ae7c308b79fb963fe1ec581482a13f5
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2024-12-13 00:47:06 -0300

    nvim: move colorscheme-related configurations into a single file

--- home/nvim/lua/plugins/fzf-lua.lua
+++ home/nvim/lua/plugins/fzf-lua.lua
@@ -1,14 +1,6 @@
-local shared = require("shared")
-
 return {
   {
     "ibhagwan/fzf-lua",
-    opts = {
-      fzf_colors = {
-        ["hl"] = shared.colors.primary,
-        ["hl+"] = shared.colors.secondary,
-      },
-    },
     keys = {
       { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
       { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
