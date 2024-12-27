40a78d20ddbc676f83cf6788c6927d499086a274
Vinicius Deolindo <git@vini.cat>
2024-12-27 07:19:06 -0300

    nvim: disable the mouse and comment the options a little better

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -1,9 +1,14 @@
+-- 4-spaces indentation
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
--- ttimeout is really annoying when paired with tmux
+-- ttimeout is really annoying
 vim.opt.ttimeout = false
 
+-- Disable the mouse
+vim.opt.mouse = ""
+
+-- Disable lsp logging, the file gets massive after a while
 vim.lsp.set_log_level("off")
 
 -- Disable snacks animations
