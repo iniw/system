5b9bb8e5ec591479f6c707e4bcd8ee6850a48896
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-01-03 00:58:42 -0300

    nvim: reduce debouce delay for `snacks.words`

--- home/nvim/lua/plugins/snacks.lua
+++ home/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,10 @@ return {
   {
     "snacks.nvim",
     opts = function(_, opts)
+      -- Remove the delay before updating
+      opts.words.debounce = 25
+
+      -- Remove the header/footer
       opts.dashboard.sections = {
         { section = "keys", gap = 1, padding = 1 },
       }
