27d43cceb8ad597443abe0e21a3027b2ad96bb63
Vinicius Deolindo <git@vini.cat>
2025-01-16 10:48:27 -0300

    nvim: use snacks.picker instead of fzf-lua

--- home/nvim/lua/plugins/snacks.lua
+++ home/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "snacks.nvim",
+    "folke/snacks.nvim",
     opts = function(_, opts)
       -- Remove the delay before updating
       opts.words.debounce = 25
@@ -10,5 +10,20 @@ return {
         { section = "keys", gap = 1, padding = 1 },
       }
     end,
+    keys = {
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      {
+        "gad",
+        function()
+          local snacks = require("snacks")
+          snacks.picker.lsp_definitions({
+            confirm = "edit_vsplit",
+          })
+        end,
+        desc = "Goto Definition in a New Window",
+      },
+    },
   },
 }
