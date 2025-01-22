0bf873f54d0227b4d7f989c954a8ca2ed7728af8
Vinicius Deolindo <git@vini.cat>
2025-01-22 13:09:13 -0300

    nvim: invert root/cwd neo-tree keybinds

--- home/nvim/lua/plugins/neo-tree.lua
+++ home/nvim/lua/plugins/neo-tree.lua
@@ -6,5 +6,23 @@ return {
         position = "right",
       },
     },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
   },
 }
