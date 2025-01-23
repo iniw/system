2c76aa69ad4d4d8c50fc8b2d9a2053430597115e
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-01-23 09:11:51 -0300

    nvim: small plugin adjustments

--- home/nvim/lua/plugins/which-key.lua
+++ home/nvim/lua/plugins/which-key.lua
@@ -3,11 +3,6 @@ return {
     "folke/which-key.nvim",
     opts = {
       preset = "modern",
-      spec = {
-        {
-          { "<leader>y", group = "yazi", icon = { icon = " ", color = "blue" } },
-        },
-      },
     },
   },
 }
