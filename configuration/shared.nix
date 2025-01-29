4c3d75c655ee93c3e9fa6ff1f515058e68514030
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-01-29 11:32:39 -0300

    configuration: separate nixos/darwin ssh keys

--- configuration/shared.nix
+++ configuration/shared.nix
@@ -43,17 +43,6 @@
     };
   };
 
-  programs = {
-    ssh = {
-      knownHosts = {
-        "github.com" = {
-          hostNames = [ "20.201.28.151" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-        };
-      };
-    };
-  };
-
   # Nix
 
   nix = {
