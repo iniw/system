18200e023637afc1608e0cb453cccbf3d9aeb5f4
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-01-29 16:27:29 -0300

    wezterm: hide tab bar if only one tab is opened

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -174,6 +174,7 @@ return {
   use_fancy_tab_bar = false,
   tab_bar_at_bottom = true,
   show_new_tab_button_in_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
 
   -- Window
   window_padding = {
