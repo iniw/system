110556a5b2748bf2836ca7a7233e93f807645d0a
Vinicius Deolindo <git@vini.cat>
2025-02-08 21:35:10 -0300

    nvim: make an `adjustments` file
    this now contains both the `no-semver.lua` and `disabled.lua` files
    
    also added more plugins to the `no-semver` list

--- home/nvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
-  { "windwp/nvim-ts-autotag", enabled = false },
-}
