18da7f1f33f80b6c83848fb784594810db4912e1
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-02-12 13:05:06 -0300

    nvim: define "on file load" events in a single place

--- home/nvim/lua/plugins/git.lua
+++ home/nvim/lua/plugins/git.lua
@@ -12,7 +12,7 @@ return {
 
   {
     "lewis6991/gitsigns.nvim",
-    event = { "BufRead", "BufNewFile" },
+    event = sol.OnFile,
     ---@module "gitsigns"
     opts = {
       on_attach = function(buffer)
