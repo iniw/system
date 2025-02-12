0db0a19e9d0ed74a01a4e0493e9e7d0e47a85950
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-02-12 19:01:59 -0300

    nvim: merge colorscheme into `ui` module

--- home/nvim/lua/plugins/ui.lua
+++ home/nvim/lua/plugins/ui.lua
@@ -1,5 +1,44 @@
 ---@type LazySpec
 return {
+  -- Colorscheme
+  {
+    "jesseleite/nvim-noirbuddy",
+    lazy = false,
+    dependencies = { { "tjdevries/colorbuddy.nvim", version = false } },
+    config = function()
+      local palette = {
+        primary = "#73b3e7",
+        primary_light = "#c5d8e8",
+      }
+
+      require("noirbuddy").setup({
+        colors = {
+          primary = palette.primary,
+        },
+      })
+
+      local cb = require("colorbuddy")
+
+      cb.Color.new("primary_light", palette.primary_light)
+
+      -- Slightly brighter text
+      cb.Group.new("Normal", cb.colors.secondary)
+      cb.Group.new("NormalFloat", cb.colors.secondary)
+
+      -- White methods
+      cb.Group.new("@method", cb.colors.white)
+      cb.Group.new("@lsp.type.method", cb.colors.white)
+
+      -- More highlights here and there
+      cb.Group.new("SnacksPickerMatch", cb.colors.primary)
+      cb.Group.new("@operator", cb.colors.primary)
+      cb.Group.new("@punctuation.bracket", cb.colors.primary_light)
+
+      -- Slightly brighter NonText, which is usually used for dimmed stuff
+      cb.Group.new("NonText", cb.groups.Comment)
+    end,
+  },
+
   {
     "folke/snacks.nvim",
     ---@type snacks.Config
