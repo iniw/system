83cefe415cc87acf210780cd09460ac79fcede4b
Vinicius Deolindo <git@vini.cat>
2025-02-19 13:07:40 -0300

    nvim: small rust stuff

--- home/nvim/lua/plugins/lang/rust.lua
+++ home/nvim/lua/plugins/lang/rust.lua
@@ -2,7 +2,7 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    ft = { "rust" },
+    ft = "rust",
     ---@module "rustaceanvim"
     ---@type rustaceanvim.Opts
     opts = {
@@ -44,7 +44,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        rust = { "rustfmt" },
+        rust = { "rustfmt", lsp_format = "prefer" },
       },
     },
   },
