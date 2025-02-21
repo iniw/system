cc615f5d9f89d7028dc4a64c901275225b166c39
Vinicius Deolindo <git@vini.cat>
2025-02-21 13:04:50 -0300

    nvim: add "jump to window with wrap" keymaps

--- home/nvim/lua/sol.lua
+++ home/nvim/lua/sol.lua
@@ -65,6 +65,24 @@ function M.send_buffer_to_split(split)
   end
 end
 
+-- From: https://stackoverflow.com/questions/13848429/window-navigation-wrap-around-in-vim
+
+---@param direction "h"|"j"|"k"|"l"
+---@param opposite "h"|"j"|"k"|"l"
+function M.jump_window_with_wrap(direction, opposite)
+  local function try_jump_window(try_direction, count)
+    local prev_win_nr = vim.fn.winnr()
+    vim.cmd(count .. "wincmd " .. try_direction)
+    return vim.fn.winnr() ~= prev_win_nr
+  end
+
+  return function()
+    if not try_jump_window(direction, vim.v.count1) then
+      try_jump_window(opposite, 999)
+    end
+  end
+end
+
 M.OnFile = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
 return M
