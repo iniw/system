10ad2119e7740173d1ed4350db5ca4d585899b74
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-04-14 20:23:29 -0300

    nvim: add a keymap to organize imports with vtsls

--- home/nvim/lua/plugins/lang/web.lua
+++ home/nvim/lua/plugins/lang/web.lua
@@ -7,6 +7,23 @@ return {
         cssls = {},
         html = {},
         vtsls = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                local ft = vim.bo.filetype:gsub("react$", "")
+                if not vim.tbl_contains({ "javascript", "typescript" }, ft) then
+                  return
+                end
+                vim.lsp.buf_request(0, "workspace/executeCommand", {
+                  command = (ft .. ".organizeImports"),
+                  arguments = { vim.api.nvim_buf_get_name(0) },
+                })
+              end,
+              desc = "Organize imports",
+            },
+          },
+
           settings = {
             vtsls = {
               autoUseWorkspaceTsdk = true,
