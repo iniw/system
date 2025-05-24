d9350fbe2efadc346c54fe594b2518bd631a602e
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-05-24 16:00:17 -0300

    home: add some treesitter injection tips to raw string literals

--- home/shared.nix
+++ home/shared.nix
@@ -224,16 +224,18 @@
       # See also: https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
       #           https://wiki.archlinux.org/title/Fish#System_integration
       #           https://wiki.gentoo.org/wiki/Fish#Caveats
-      initContent = ''
-        if [[ ! $(ps -T -o "comm" | tail -n +2 | grep "nu$") && -z $ZSH_EXECUTION_STRING ]]; then
-            if [[ -o login ]]; then
-                LOGIN_OPTION='--login'
-            else
-                LOGIN_OPTION='''
-            fi
-            exec nu "$LOGIN_OPTION"
-        fi
-      '';
+      initContent =
+        # sh
+        ''
+          if [[ ! $(ps -T -o "comm" | tail -n +2 | grep "nu$") && -z $ZSH_EXECUTION_STRING ]]; then
+              if [[ -o login ]]; then
+                  LOGIN_OPTION='--login'
+              else
+                  LOGIN_OPTION='''
+              fi
+              exec nu "$LOGIN_OPTION"
+          fi
+        '';
     };
   };
 
