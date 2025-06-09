61a101835244c1c69c9cebe2bc7e7a0a8a7e191a
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-06-09 10:21:09 -0300

    hm/helix+yazi: add keymap to use yazi in helix

--- home/shared.nix
+++ home/shared.nix
@@ -261,6 +261,19 @@
     yazi = {
       enable = true;
       package = pkgs-unstable.yazi;
+
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
+            on = [
+              "g"
+              "r"
+            ];
+            desc = "Go to git root";
+          }
+        ];
+      };
     };
 
     zoxide = {
