54e110c31f07bea51b867fa31ec14a45934d6d23
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-07-28 14:07:08 -0300

    flake: use a single `nixpkgs` input for 25.05

--- flake.nix
+++ flake.nix
@@ -2,18 +2,17 @@
   description = "wini's system flake";
 
   inputs = {
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin/nix-darwin-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
@@ -30,27 +29,25 @@
 
     mac-app-util = {
       url = "github:hraban/mac-app-util";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     klip = {
       url = "github:iniw/klip";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     fonts = {
       url = "git+ssh://git@github.com/iniw/fonts";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
     inputs:
     let
-      lib = inputs.nixpkgs-unstable.lib;
-      sys = import ./lib/sys.nix {
-        inherit inputs lib;
-      };
+      lib = inputs.nixpkgs.lib;
+      sys = import ./lib/sys.nix inputs;
 
       configurations =
         builtins.readDir ./hosts
