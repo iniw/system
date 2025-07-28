497180ed8ebaa33eec7edfc8d9eac69f421dda5b
Vinicius Deolindo <git@vini.cat>
2025-07-28 16:31:01 -0300

    modules: consolidate `postgresql` and `mysql` into a single module

--- modules/sql/default.nix
+++ modules/sql/default.nix
@@ -6,10 +6,16 @@
         restart_service = "changed";
         conflicts_with = [ name ];
       }
+      rec {
+        name = "postgresql@17";
+        restart_service = "changed";
+        conflicts_with = [ name ];
+      }
     ];
   };
 
   nixosSystemModule = {
     services.mysql.enable = true;
+    services.postgresql.enable = true;
   };
 }
