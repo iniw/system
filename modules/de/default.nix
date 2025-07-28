d67bf3fa0c5e255896c5cb75372912b93de8a15e
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-07-28 17:04:48 -0300

    modules: remove the `gnome` module
    it was replaced by `de` but i fogor to remove it

--- modules/de/default.nix
+++ modules/de/default.nix
@@ -1,49 +1,4 @@
 {
-  nixosSystemModule = {
-    services.xserver = {
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-    };
-  };
-
-  nixosHomeModule =
-    { lib, ... }:
-    {
-      dconf = {
-        enable = true;
-
-        settings = {
-          "org/gnome/desktop/input-sources" = {
-            xkb-options = [ "caps:escape" ];
-          };
-
-          "org/gnome/desktop/interface" = {
-            color-scheme = "prefer-dark";
-            font-hinting = "full";
-            font-antialiasing = "rgba";
-          };
-
-          "org/gnome/desktop/peripherals/keyboard" = {
-            delay = lib.hm.gvariant.mkUint32 200;
-            repeat = lib.hm.gvariant.mkUint32 20;
-          };
-
-          "org/gnome/desktop/session" = {
-            idle-delay = lib.hm.gvariant.mkUint32 0;
-          };
-
-          "org/gnome/shell" = {
-            favorite-apps = [
-              "google-chrome.desktop"
-              "com.mitchellh.ghostty.desktop"
-              "discord.desktop"
-              "spotify.desktop"
-            ];
-          };
-        };
-      };
-    };
-
   darwinSystemModule = {
     security.pam.services.sudo_local.touchIdAuth = true;
 
@@ -99,4 +54,49 @@
       };
     };
   };
+
+  nixosSystemModule = {
+    services.xserver = {
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+  };
+
+  nixosHomeModule =
+    { lib, ... }:
+    {
+      dconf = {
+        enable = true;
+
+        settings = {
+          "org/gnome/desktop/input-sources" = {
+            xkb-options = [ "caps:escape" ];
+          };
+
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+            font-hinting = "full";
+            font-antialiasing = "rgba";
+          };
+
+          "org/gnome/desktop/peripherals/keyboard" = {
+            delay = lib.hm.gvariant.mkUint32 200;
+            repeat = lib.hm.gvariant.mkUint32 20;
+          };
+
+          "org/gnome/desktop/session" = {
+            idle-delay = lib.hm.gvariant.mkUint32 0;
+          };
+
+          "org/gnome/shell" = {
+            favorite-apps = [
+              "google-chrome.desktop"
+              "com.mitchellh.ghostty.desktop"
+              "discord.desktop"
+              "spotify.desktop"
+            ];
+          };
+        };
+      };
+    };
 }
