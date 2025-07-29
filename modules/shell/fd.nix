a2b00e576770e3412f955a5980f4d2616c6184ec
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-07-29 15:31:19 -0300

    shell+vcs: color more outputs and add a better alias for `less`

--- modules/shell/fd.nix
+++ modules/shell/fd.nix
@@ -4,9 +4,11 @@
       fd.enable = true;
 
       # Use fd instead of find.
-      fzf.changeDirWidgetCommand = ''fd --type d --hidden --follow --exclude ".git" --exclude ".jj"'';
-      fzf.defaultCommand = ''fd --hidden --follow --exclude ".git" --exclude ".jj"'';
-      fzf.fileWidgetCommand = ''fd --hidden --follow --exclude ".git" --exclude ".jj"'';
+      fzf = {
+        changeDirWidgetCommand = ''fd --type d --hidden --follow --exclude ".git" --exclude ".jj"'';
+        defaultCommand = ''fd --hidden --follow --exclude ".git" --exclude ".jj"'';
+        fileWidgetCommand = ''fd --hidden --follow --exclude ".git" --exclude ".jj"'';
+      };
 
       git.ignores = [ ".ignore" ];
     };
