51c285c55905b48c6f209c720f317547205b4d85
Vinicius Deolindo <git@vini.cat>
2025-08-25 11:46:22 -0300

    ssh: add sourcehut host

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -7,6 +7,14 @@
     programs.ssh = {
       enable = true;
       addKeysToAgent = "confirm";
+
+      matchBlocks = {
+        # https://man.sr.ht/tutorials/set-up-account-and-git.md#specifying-a-key
+        "*sr.ht" = {
+          identityFile = "~/.ssh/id_ed25519.sourcehut";
+          extraOptions.PreferredAuthentications = "publickey";
+        };
+      };
     };
   };
 
