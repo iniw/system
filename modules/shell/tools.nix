2c32299e62b3a8d109ea69724684e1230d2e8f21
Vinicius Deolindo <git@vini.cat>
2025-09-09 15:34:34 -0300

    shell: don't use the gnu family of `man` commands on darwin

--- modules/shell/tools.nix
+++ modules/shell/tools.nix
@@ -47,4 +47,12 @@
         zoxide.enable = true;
       };
     };
+
+  darwinHomeModule = {
+    # Don't override darwin's built-in `man` program with GNU's version while also installing the manpages for hm-managed programs.
+    # This does essentially the same thing as the `man` module without adding the `package` to `home.packages`.
+    # See: https://github.com/nix-community/home-manager/blob/26993d87fd0d3b14f7667b74ad82235f120d986e/modules/programs/man.nix#L44-L45
+    home.extraOutputsToInstall = [ "man" ];
+    programs.man.enable = false;
+  };
 }
