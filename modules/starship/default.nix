59fe6240444242f8d1d51e344c0fbcabff2714f3
Vinicius Deolindo <git@vini.cat>
2025-09-19 15:28:08 -0300

    starship: configure with nix

--- modules/starship/default.nix
+++ modules/starship/default.nix
@@ -1,6 +1,54 @@
 {
-  homeModule = {
-    programs.starship.enable = true;
-    xdg.configFile."starship.toml".source = ./config/starship.toml;
-  };
+  homeModule =
+    { lib, ... }:
+    {
+      programs.starship = {
+        enable = true;
+        settings = {
+          format =
+            let
+              modules = lib.concatStrings [
+                "$time"
+                "$directory"
+                "$nix_shell"
+                "$cmd_duration"
+                "$status"
+              ];
+            in
+            ''
+              ┌${modules}
+              └❯'';
+
+          time = {
+            format = ''\[[$time]($style)\]'';
+            time_format = "%H:%M";
+            style = "cyan";
+            disabled = false;
+          };
+
+          directory = {
+            format = ''─\[[$path]($style)[$read_only]($read_only_style)\]'';
+            style = "blue";
+            truncate_to_repo = false;
+            read_only = "*";
+          };
+
+          cmd_duration = {
+            format = ''─\[[$duration]($style)\]'';
+            style = "yellow";
+          };
+
+          status = {
+            format = ''─\[[$status]($style)\]'';
+            disabled = false;
+            map_symbol = true;
+          };
+
+          nix_shell = {
+            format = ''─\[[$name]($style)\]'';
+            style = "bright-black";
+          };
+        };
+      };
+    };
 }
