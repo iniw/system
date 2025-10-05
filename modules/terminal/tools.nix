a31019c78e4b3199342cd772547811b55d57ad4c
Vinicius Deolindo <git@vini.cat>
2025-10-05 20:00:58 -0300

    yazi: move yazi-specific packages like `exiftool` to `extraPackages`

--- modules/terminal/tools.nix
+++ modules/terminal/tools.nix
@@ -5,7 +5,6 @@
       home = {
         packages = with pkgs; [
           ast-grep
-          exiftool
           hyperfine
           jq
           nh
--- modules/terminal/yazi.nix
+++ modules/terminal/yazi.nix
@@ -1,32 +1,39 @@
 {
-  homeManagerModule = {
-    programs = {
-      yazi = {
-        enable = true;
+  homeManagerModule =
+    { pkgs, ... }:
+    {
+      programs = {
+        yazi = {
+          enable = true;
 
-        keymap.mgr.prepend_keymap = [
-          {
-            run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
-            on = [
-              "g"
-              "r"
-            ];
-            desc = "Go to git root";
-          }
-        ];
-      };
+          extraPackages = with pkgs; [
+            exiftool
+            poppler
+          ];
 
-      helix.settings.keys = {
-        normal.ret.e = [
-          '':sh rm -f /tmp/yazi-chooser-file''
-          '':insert-output yazi %{buffer_name} --chooser-file=/tmp/yazi-chooser-file''
-          '':insert-output echo "[?1049h[?2004h" > /dev/tty''
-          '':open %sh{cat /tmp/yazi-chooser-file}''
-          '':redraw''
-          '':set mouse false''
-          '':set mouse true''
-        ];
+          keymap.mgr.prepend_keymap = [
+            {
+              run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
+              on = [
+                "g"
+                "r"
+              ];
+              desc = "Go to git root";
+            }
+          ];
+        };
+
+        helix.settings.keys = {
+          normal.ret.e = [
+            '':sh rm -f /tmp/yazi-chooser-file''
+            '':insert-output yazi %{buffer_name} --chooser-file=/tmp/yazi-chooser-file''
+            '':insert-output echo "[?1049h[?2004h" > /dev/tty''
+            '':open %sh{cat /tmp/yazi-chooser-file}''
+            '':redraw''
+            '':set mouse false''
+            '':set mouse true''
+          ];
+        };
       };
     };
-  };
 }
