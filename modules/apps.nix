add48757f765671d525f878a3d524884133a3192
Vinicius Deolindo <git@vini.cat>
2025-10-07 13:00:55 -0300

    apps: use `firefox-bin` instead of `firefox`

--- modules/apps.nix
+++ modules/apps.nix
@@ -2,9 +2,15 @@
   homeManagerModule =
     { pkgs, ... }:
     {
-      home.packages = with pkgs; [
-        discord
-        firefox
-      ];
+      home.packages =
+        with pkgs;
+        let
+          # `firefox-bin` is not properly codesigned on darwin
+          firefox = if stdenv.isDarwin then firefox-bin-unwrapped else firefox-bin;
+        in
+        [
+          discord
+          firefox
+        ];
     };
 }
