e742a9656e90961d0baafa33235047c2e91368ab
Vinicius Deolindo <git@vini.cat>
2025-10-25 03:44:24 -0300

    sol: cleanup apple defaults

--- hosts/sol/modules/desktop.nix
+++ hosts/sol/modules/desktop.nix
@@ -4,50 +4,74 @@
     {
       security.pam.services.sudo_local.touchIdAuth = true;
 
-      system = {
-        defaults = {
-          NSGlobalDomain = {
-            AppleKeyboardUIMode = 3;
-            ApplePressAndHoldEnabled = false;
-            AppleShowAllFiles = true;
-
-            NSAutomaticCapitalizationEnabled = false;
-            NSAutomaticDashSubstitutionEnabled = false;
-            NSAutomaticPeriodSubstitutionEnabled = false;
-            NSAutomaticQuoteSubstitutionEnabled = false;
-            NSAutomaticSpellingCorrectionEnabled = false;
-
-            NSNavPanelExpandedStateForSaveMode = true;
-            NSNavPanelExpandedStateForSaveMode2 = true;
+      system.defaults = {
+        CustomSystemPreferences = {
+          "com.apple.AdLib" = {
+            allowApplePersonalizedAdvertising = false;
+            allowIdentifierForAdvertising = false;
+            forceLimitAdTracking = true;
+            personalizedAdsMigrated = false;
           };
 
-          dock = {
-            autohide = true;
-            mru-spaces = false;
-            show-recents = false;
-            orientation = "bottom";
-            showhidden = true;
-            tilesize = 32;
-            persistent-apps = [
-              { app = "/Users/${user}/Applications/Home Manager Apps/Ghostty.app"; }
-              { app = "/Users/${user}/Applications/Home Manager Apps/Firefox.app"; }
-              { app = "/Users/${user}/Applications/Home Manager Apps/Discord.app"; }
-              { app = "/Applications/Qobuz.app"; }
-              { app = "/Users/${user}/Applications/Home Manager Apps/Thunderbird.app"; }
-              { app = "/Applications/Slack.app"; }
-              { app = "/Applications/Linear.app"; }
-            ];
+          "com.apple.finder" = {
+            FXArrangeGroupViewBy = "Name";
           };
+        };
 
-          finder = {
-            AppleShowAllExtensions = true;
-            AppleShowAllFiles = true;
+        dock = {
+          autohide = true;
+          mru-spaces = false;
+          show-recents = false;
+          orientation = "bottom";
+          showhidden = true;
+          tilesize = 32;
+          persistent-apps = [
+            { app = "/Users/${user}/Applications/Home Manager Apps/Ghostty.app"; }
+            { app = "/Users/${user}/Applications/Home Manager Apps/Firefox.app"; }
+            { app = "/Users/${user}/Applications/Home Manager Apps/Discord.app"; }
+            { app = "/Applications/Qobuz.app"; }
+            { app = "/Users/${user}/Applications/Home Manager Apps/Thunderbird.app"; }
+            { app = "/Applications/Slack.app"; }
+            { app = "/Applications/Linear.app"; }
+          ];
+        };
 
-            FXEnableExtensionChangeWarning = false;
-            FXPreferredViewStyle = "clmv";
+        finder = {
+          AppleShowAllExtensions = true;
+          AppleShowAllFiles = true;
 
-            QuitMenuItem = true;
-          };
+          _FXSortFoldersFirst = true;
+          FXDefaultSearchScope = "SCcf";
+          FXEnableExtensionChangeWarning = false;
+          FXPreferredViewStyle = "Nlsv";
+          FXRemoveOldTrashItems = true;
+
+          NewWindowTarget = "Home";
+
+          ShowPathbar = true;
+
+          QuitMenuItem = true;
+        };
+
+        LaunchServices = {
+          LSQuarantine = false;
+        };
+
+        NSGlobalDomain = {
+          ApplePressAndHoldEnabled = false;
+          AppleShowAllFiles = true;
+
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+
+          NSDocumentSaveNewDocumentsToCloud = false;
+        };
+
+        screencapture = {
+          target = "clipboard";
         };
       };
     };
