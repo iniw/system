e5adb555080d9eee3e9d9e27e56e5b4134371b42
Vinicius Deolindo <git@vini.cat>
2025-10-29 18:43:55 -0300

    modules+hosts: make the `input` module into host-specific

--- hosts/lua/modules/input.nix
+++ hosts/lua/modules/input.nix
@@ -1,16 +1,36 @@
 {
+  systemModule = {
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      inputMethod = {
+        enable = true;
+        type = "ibus";
+      };
+    };
+  };
+
   homeManagerModule =
     { lib, ... }:
     {
       dconf.settings = {
-        "org/gnome/desktop/input-sources".sources = [
-          (lib.hm.gvariant.mkTuple [
-            "xkb"
-            "br"
-          ])
-        ];
+        "org/gnome/desktop/input-sources" = {
+          xkb-options = [ "caps:escape" ];
+          sources = [
+            (lib.hm.gvariant.mkTuple [
+              "xkb"
+              "br"
+            ])
+          ];
+        };
 
-        "org/gnome/desktop/peripherals/mouse".speed = -0.20;
+        "org/gnome/desktop/peripherals/keyboard" = {
+          delay = lib.hm.gvariant.mkUint32 200;
+          repeat = lib.hm.gvariant.mkUint32 20;
+        };
+
+        "org/gnome/desktop/peripherals/mouse" = {
+          speed = -0.20;
+        };
       };
     };
 }
---
+++ hosts/sol/modules/input.nix
@@ -0,0 +1,22 @@
+{
+  systemModule = {
+    system = {
+      defaults = {
+        NSGlobalDomain = {
+          InitialKeyRepeat = 12;
+          KeyRepeat = 2;
+        };
+
+        trackpad = {
+          Clicking = true;
+          TrackpadThreeFingerDrag = true;
+        };
+      };
+
+      keyboard = {
+        enableKeyMapping = true;
+        remapCapsLockToEscape = true;
+      };
+    };
+  };
+}
