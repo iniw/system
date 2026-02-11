95817496a82f65bf5a26a9c8028e1d28a5d2be26
Vinicius Deolindo <git@vini.cat>
2026-02-10 21:24:07 -0300

    ai: remove `amp-cli`

--- modules/ai.nix
+++ modules/ai.nix
@@ -4,13 +4,10 @@
     {
       home.packages = with pkgs; [
         claude-code
-        amp-cli
       ];
 
       programs.git.ignores = [
         ".claude/"
-        "CLAUDE.md"
-        "AGENTS.md"
       ];
     };
 }
