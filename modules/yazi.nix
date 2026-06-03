a6106b0e194f2bd0a3f3b21e4eb8c9ca2419cd2d
Vinicius Deolindo <git@vini.cat>
2026-06-03 12:54:25 -0300

    yazi: remove unnecessary `extraPackages`

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,48 +1,41 @@
 {
-  homeManagerModule =
-    { pkgs, ... }:
-    {
-      programs = {
-        yazi = {
-          enable = true;
+  homeManagerModule = {
+    programs = {
+      yazi = {
+        enable = true;
 
-          extraPackages = with pkgs; [
-            exiftool
-            poppler
-          ];
+        settings.mgr.show_hidden = true;
 
-          settings.mgr.show_hidden = true;
+        keymap.mgr.prepend_keymap = [
+          {
+            run = ''shell -- ya emit cd "$(jj workspace root)"'';
+            on = [
+              "g"
+              "r"
+            ];
+            desc = "Go to repository root";
+          }
+        ];
+      };
 
-          keymap.mgr.prepend_keymap = [
-            {
-              run = ''shell -- ya emit cd "$(jj workspace root)"'';
-              on = [
-                "g"
-                "r"
-              ];
-              desc = "Go to repository root";
-            }
-          ];
-        };
-
-        helix.settings.keys = {
-          normal.ret =
-            let
-              open-yazi = expansion: [
-                ":sh rm -f /tmp/yazi-chooser-file"
-                ":insert-output yazi %{${expansion}} --chooser-file=/tmp/yazi-chooser-file"
-                '':insert-output echo "[?1049h[?2004h" > /dev/tty''
-                ":open %sh{cat /tmp/yazi-chooser-file}"
-                ":redraw"
-                ":set mouse false"
-                ":set mouse true"
-              ];
-            in
-            {
-              e = open-yazi "buffer_name";
-              S-e = open-yazi "workspace_directory";
-            };
-        };
+      helix.settings.keys = {
+        normal.ret =
+          let
+            open-yazi = expansion: [
+              ":sh rm -f /tmp/yazi-chooser-file"
+              ":insert-output yazi %{${expansion}} --chooser-file=/tmp/yazi-chooser-file"
+              '':insert-output echo "[?1049h[?2004h" > /dev/tty''
+              ":open %sh{cat /tmp/yazi-chooser-file}"
+              ":redraw"
+              ":set mouse false"
+              ":set mouse true"
+            ];
+          in
+          {
+            e = open-yazi "buffer_name";
+            S-e = open-yazi "workspace_directory";
+          };
       };
     };
+  };
 }
