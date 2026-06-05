cda96b2f4407ec99ffd1adace503abf4d3bc325d
Vinicius Deolindo <git@vini.cat>
2026-06-05 20:22:29 -0300

    sol/default: bump home-manager's `stateVersion` to `26.11`

--- hosts/sol/default.nix
+++ hosts/sol/default.nix
@@ -28,7 +28,7 @@ sys.darwinSystem (
         </plist>
       '';
 
-    home-manager.users.${user}.home.stateVersion = "26.05";
+    home-manager.users.${user}.home.stateVersion = "26.11";
     system.stateVersion = 5;
   }
 )
