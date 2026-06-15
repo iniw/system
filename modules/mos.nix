231d66793fdb9a6ea49b6a5d47a68c9bdf9f850e
Vinicius Deolindo <git@vini.cat>
2026-06-14 22:23:10 -0300

    mos: 4.2.0 -> 4.2.1

--- modules/mos.nix
+++ modules/mos.nix
@@ -3,10 +3,10 @@
     nixpkgs.overlays = [
       (final: prev: {
         mos = prev.mos.overrideAttrs rec {
-          version = "4.2.0";
+          version = "4.2.1";
           src = final.fetchurl {
-            url = "https://github.com/Caldis/Mos/releases/download/${version}/Mos.Versions.${version}-20260505.1.zip";
-            hash = "sha256-SswAG7V+7LbAbPcHwS4Kr+0TFg6XWhYTEK7lVC3lYCQ=";
+            url = "https://github.com/Caldis/Mos/releases/download/${version}/Mos.Versions.${version}-20260531.1.zip";
+            hash = "sha256-LqaelvCS5E2tqTpVvaHN2rMynFJ7vV8G4A37eOlTlgo=";
           };
         };
       })
