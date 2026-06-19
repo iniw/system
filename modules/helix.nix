66ad9c66d533059794ebea3567aa706c92cc8495
Vinicius Deolindo <git@vini.cat>
2026-06-19 19:24:11 -0300

    helix: `workspace-trust.globs` -> `workspace-trust.trusted`

--- modules/helix.nix
+++ modules/helix.nix
@@ -66,7 +66,7 @@
               };
 
               workspace-trust = {
-                globs = [
+                trusted = [
                   "~/work/**"
                   "~/dev/**"
                 ];
