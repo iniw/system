8b6e99bb34aeb3634bc5fa2c10f47d01c18d3bad
Vinicius Deolindo <git@vini.cat>
2026-07-14 14:12:28 -0300

    sol: use `nix-darwin`'s new `program.mas` option

--- hosts/sol/modules/apps.nix
+++ hosts/sol/modules/apps.nix
@@ -1,4 +1,17 @@
 {
+  systemModule = {
+    programs.mas = {
+      enable = true;
+
+      packages = {
+        FastScrobbler = 6759501541;
+        wBlock = 6746388723;
+        WhatsApp = 310633997;
+        Xcode = 497799835;
+      };
+    };
+  };
+
   homeManagerModule =
     { pkgs, ... }:
     {
