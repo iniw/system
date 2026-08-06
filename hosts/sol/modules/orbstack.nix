5e5287286625bcf35fcc20454c4b02d0134a3a3f
Vinicius Deolindo <git@vini.cat>
2026-08-05 22:32:23 -0300

    orbstack: remove `sessionPath` hack now that v2.2.3 is released

--- hosts/sol/modules/orbstack.nix
+++ hosts/sol/modules/orbstack.nix
@@ -1,13 +1,6 @@
 {
   homeManagerModule = { pkgs, ... }: {
+    home.packages = [ pkgs.orbstack ];
     programs.ssh.includes = [ "~/.orbstack/ssh/config" ];
-
-    home = {
-      packages = [ pkgs.orbstack ];
-
-      # Keep the CLI and GUI on the same app copy to avoid restarting the service.
-      # See https://github.com/orbstack/orbstack/issues/2614
-      sessionPath = [ "$HOME/.orbstack/bin" ];
-    };
   };
 }
