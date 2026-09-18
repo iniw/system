0bd2bc500f52cf0551d8729f4edc867065ea1089
Vinicius Deolindo <git@vini.cat>
2026-09-18 19:29:53 -0300

    lua: pack nested `boot` configs

--- hosts/lua/default.nix
+++ hosts/lua/default.nix
@@ -2,15 +2,16 @@ sys:
 sys.nixos (
   { user, pkgs, ... }:
   {
-    boot.loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+
+      kernelPackages = pkgs.linuxPackages_latest;
     };
 
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-
     home-manager.users.${user}.home.stateVersion = "26.11";
-
     system.stateVersion = "26.11";
   }
 )
